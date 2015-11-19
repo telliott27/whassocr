a337764bac972a61677a26e2ecdfb300996a483c
Thomas Elliott <telliott@fastmail.fm>
2015-11-19 10:53:16 -0700

    Updated lar documentation.

--- R/lagged_association.R
+++ R/lagged_association.R
@@ -4,7 +4,7 @@
 #'
 #' @param x a m X n observation matrix
 #' @param group an integer vector of length m indicating the time grouping of the observations.
-#'  Group values should be sequential with no gaps.
+#'  Group values should be sequential with no gaps. The vector does not need to be sorted.
 #' @param t the order of the lag
 #' @param cutoff the number of times above which two entities must be observed together within
 #'  a single time group to count as being associated
@@ -50,29 +50,9 @@ lar<-function(x,group,t=1,cutoff=0) {
   return(g)
 }
 
-#' Null Lagged Association Rate
-#'
-#' Calculate the null lagged association rate for a two-mode observation matrix. This is the
-#' expected lagged association rate if entities associated randomly.
-#'
-#' @param x a m X n observation matrix
-#' @param group an integer vector of length m indicating the time grouping of the observations.
-#'  Group values should be sequential with no gaps.
-#' @param t the order of the lag
-#' @param cutoff the number of times above which two entities must be observed together within
-#'  a single time group to count as being associated
-#'
-#' @details The grouping variable \code{group} is used to aggregate observational data to
-#'  daily, weekly, yearly, etc., rates of association. This allows you to calculate a lagged association
-#'  rate between two months or two years.
-#'
-#'  \code{cutoff} is only useful if you group observations, and allows you to set a minimum number of times
-#'  two entities must be observed together in order to count as being associated. Note that two entities must
-#'  be observed together MORE THAN the value of \code{cutoff}
-#'
-#'  @return The null lagged association rate for the observation matrix for a lag of \code{t}
 
-nulllar<-function(x,group,t=1,cutoff=0) {
+#' @describeIn lar Calculate the null lagged association rate
+null.lar<-function(x,group,t=1,cutoff=0) {
   times<-group-min(group)+1
   sumN<-0
   sumD<-0
